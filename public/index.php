3234f6491e4d6953749f7a3fb89101d49ab7e099
twilroad <269044570@qq.com>
2016-10-21 14:21:09 +0800

    修正bootstrap编译目录错误的问题

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
  */
 define('NOTADD_START', microtime(true));
 require __DIR__ . '/../vendor/autoload.php';
-$compiledPath = __DIR__ . '/../caches/compiled.php';
+$compiledPath = __DIR__ . '/../storage/bootstraps/compiled.php';
 if(file_exists($compiledPath)) {
     require $compiledPath;
 }

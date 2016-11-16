c37efdf4e96ac8af0df2e8cec4b7463546c0c7cc
twilroad <269044570@qq.com>
2016-11-16 18:54:18 +0800

    实现调试模式的基本配置，btn-group样式初步实现

--- src/Foundation/Debug/Apis/DebugApi.php
+++ src/Foundation/Debug/Apis/DebugApi.php
@@ -8,6 +8,7 @@
 namespace Notadd\Foundation\Debug\Apis;
 use Notadd\Foundation\Passport\Responses\ApiResponse;
 use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 /**
  * Class DebugApi
  * @package Notadd\Foundation\Debug\Apis
@@ -15,8 +16,12 @@ use Notadd\Foundation\Routing\Abstracts\Controller;
 class DebugApi extends Controller {
     /**
      * @param \Notadd\Foundation\Passport\Responses\ApiResponse $response
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     * @return \Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
      */
-    public function handle(ApiResponse $response) {
-
+    public function handle(ApiResponse $response, SettingsRepository $settings) {
+        $settings->set("debug.enabled", $this->request->input('enabled'));
+        $response->withParams($settings->all()->toArray());
+        return $response->generateHttpResponse();
     }
 }

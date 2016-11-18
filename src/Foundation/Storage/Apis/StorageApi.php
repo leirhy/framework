023da382ef1bce8fe00350d045f6db947e3b91a1
twilroad <269044570@qq.com>
2016-11-18 13:57:24 +0800

    添加Storage组件，并实现相应的设置功能

---
+++ src/Foundation/Storage/Apis/StorageApi.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-17 19:22
+ */
+namespace Notadd\Foundation\Storage\Apis;
+
+use Notadd\Foundation\Passport\Responses\ApiResponse;
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
+
+class StorageApi extends Controller
+{
+    /**
+     * @param \Notadd\Foundation\Passport\Responses\ApiResponse       $response
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function handle(ApiResponse $response, SettingsRepository $settings)
+    {
+        $settings->set('storage.default', $this->request->input('default'));
+        $response->withParams($settings->all()->toArray());
+        return $response->generateHttpResponse();
+    }
+
+}
---
+++ src/Foundation/Storage/StorageServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-17 17:59
+ */
+namespace Notadd\Foundation\Storage;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class StorageServiceProvider
+ */
+class StorageServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+}

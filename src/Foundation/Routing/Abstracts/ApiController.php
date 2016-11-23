475acc90f31e48c064a701dc36f91251b5e2fcb2
twilroad <269044570@qq.com>
2016-11-23 14:12:11 +0800

    构建ApiController的抽象基类……

---
+++ src/Foundation/Routing/Abstracts/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-23 14:07
+ */
+namespace Notadd\Foundation\Routing\Abstracts;
+
+/**
+ * Class ApiController.
+ */
+abstract class ApiController extends Controller
+{
+    protected $client;
+
+    /**
+     * ApiController constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @param $handler
+     *
+     * @return mixed
+     */
+    public function send($handler) {
+
+    }
+}

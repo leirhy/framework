26b84f380d7174d8b9bae004b1173d59ea06ac40
twilroad <269044570@qq.com>
2016-11-23 16:28:15 +0800

    基于Handler的形式重构MailController

--- src/Foundation/Mail/Controllers/MailController.php
+++ src/Foundation/Mail/Controllers/MailController.php
@@ -8,28 +8,35 @@
  */
 namespace Notadd\Foundation\Mail\Controllers;
 
-use Notadd\Foundation\Passport\Responses\ApiResponse;
+use Notadd\Foundation\Mail\Handlers\SetHandler;
 use Notadd\Foundation\Routing\Abstracts\Controller;
 use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
 class MailController extends Controller
 {
     /**
-     * @param \Notadd\Foundation\Passport\Responses\ApiResponse       $response
-     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $settings;
+
+    /**
+     * WatermarkController constructor.
      *
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     */
+    public function __construct(SettingsRepository $settings)
+    {
+        parent::__construct();
+        $this->settings = $settings;
+    }
+
+    /**
      * @return \Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
      */
-    public function handle(ApiResponse $response, SettingsRepository $settings)
+    public function handle()
     {
-        $settings->set('mail.protocol', $this->request->input('protocol'));
-        $settings->set('mail.encryption', $this->request->input('encryption'));
-        $settings->set('mail.port', $this->request->input('port'));
-        $settings->set('mail.host', $this->request->input('host'));
-        $settings->set('mail.mail', $this->request->input('mail'));
-        $settings->set('mail.username', $this->request->input('username'));
-        $settings->set('mail.password', $this->request->input('password'));
-        $response->withParams($settings->all()->toArray());
+        $handler = new SetHandler($this->container, $this->settings);
+        $response = $handler->toResponse($this->request);
 
         return $response->generateHttpResponse();
     }
---
+++ src/Foundation/Mail/Handlers/SetHandler.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-23 16:25
+ */
+namespace Notadd\Foundation\Mail\Handlers;
+
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Notadd\Foundation\Passport\Abstracts\SetHandler as AbstractSetHandler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
+
+/**
+ * Class SetHandler.
+ */
+class SetHandler extends AbstractSetHandler
+{
+    /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $settings;
+
+    /**
+     * SetHandler constructor.
+     *
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     */
+    public function __construct(Container $container, SettingsRepository $settings)
+    {
+        parent::__construct($container);
+        $this->settings = $settings;
+    }
+
+    /**
+     * @return array
+     */
+    public function data()
+    {
+        return $this->settings->all()->toArray();
+    }
+
+    /**
+     * @return array
+     */
+    public function errors()
+    {
+        return [
+            '修改设置失败！',
+        ];
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return bool
+     */
+    public function execute(Request $request)
+    {
+        $this->settings->set('mail.protocol', $request->input('protocol'));
+        $this->settings->set('mail.encryption', $request->input('encryption'));
+        $this->settings->set('mail.port', $request->input('port'));
+        $this->settings->set('mail.host', $request->input('host'));
+        $this->settings->set('mail.mail', $request->input('mail'));
+        $this->settings->set('mail.username', $request->input('username'));
+        $this->settings->set('mail.password', $request->input('password'));
+
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            '修改设置成功!',
+        ];
+    }
+
+}

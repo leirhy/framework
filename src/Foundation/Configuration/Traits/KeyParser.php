4c6d61b271da1ff31c0c9a9d2457a4c0d81d9807
twilroad <269044570@qq.com>
2016-12-01 14:47:49 +0800

    恢复Subtree测试删除的内容……

---
+++ src/Foundation/Configuration/Traits/KeyParser.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-30 17:42
+ */
+namespace Notadd\Foundation\Configuration\Traits;
+
+/**
+ * Class KeyParser.
+ */
+trait KeyParser
+{
+    /**
+     * @var array
+     */
+    protected $keyParserCache = [];
+
+    /**
+     * @param string $key
+     * @param array  $parsed
+     *
+     * @return void
+     */
+    public function setParsedKey($key, $parsed)
+    {
+        $this->keyParserCache[$key] = $parsed;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return array
+     */
+    public function parseKey($key)
+    {
+        if (isset($this->keyParserCache[$key])) {
+            return $this->keyParserCache[$key];
+        }
+        $segments = explode('.', $key);
+        if (strpos($key, '::') === false) {
+            $parsed = $this->keyParserParseBasicSegments($segments);
+        } else {
+            $parsed = $this->keyParserParseSegments($key);
+        }
+        return $this->keyParserCache[$key] = $parsed;
+    }
+
+    /**
+     * @param array $segments
+     *
+     * @return array
+     */
+    protected function keyParserParseBasicSegments(array $segments)
+    {
+        $group = $segments[0];
+        if (count($segments) == 1) {
+            return [null, $group, null];
+        }
+        else {
+            $item = implode('.', array_slice($segments, 1));
+
+            return [null, $group, $item];
+        }
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return array
+     */
+    protected function keyParserParseSegments($key)
+    {
+        list($namespace, $item) = explode('::', $key);
+        $itemSegments = explode('.', $item);
+        $groupAndItem = array_slice($this->keyParserParseBasicSegments($itemSegments), 1);
+
+        return array_merge([$namespace], $groupAndItem);
+    }
+}

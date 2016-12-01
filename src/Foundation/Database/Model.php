9eece11a2bfd4c64e0a17a1b68f7e9c623668500
twilroad <269044570@qq.com>
2016-12-01 20:16:24 +0800

    构建类Model

---
+++ src/Foundation/Database/Model.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-12-01 20:14
+ */
+namespace Notadd\Foundation\Database;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+/**
+ * Class Model.
+ */
+class Model extends EloquentModel
+{
+}

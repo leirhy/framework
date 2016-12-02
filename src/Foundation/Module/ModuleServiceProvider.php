ae717ba7751e0caf2f6f6f2930afb9c329bb1cdb
twilroad <269044570@qq.com>
2016-12-02 20:31:05 +0800

    构建类ModuleServiceProvider

---
+++ src/Foundation/Module/ModuleServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-12-02 20:29
+ */
+namespace Notadd\Foundation\Module;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ModuleServiceProvider.
+ */
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function register()
+    {
+    }
+}

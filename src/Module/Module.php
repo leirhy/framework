79dfc931b336ca9d00a18460dc0e0013bf768cab
twilroad <269044570@qq.com>
2016-12-15 17:13:25 +0800

    make class Module

--- src/Module/Module.php
+++ src/Module/Module.php
@@ -13,4 +13,100 @@ namespace Notadd\Foundation\Module;
  */
 class Module
 {
+    /**
+     * @var string|array
+     */
+    protected $author;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var bool
+     */
+    protected $installed;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Module constructor.
+     *
+     * @param string $name
+     * @param string|array $author
+     * @param string $description
+     */
+    public function __construct($name = null, $author = null, $description = null)
+    {
+        $this->author = $author;
+        $this->description = $description;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|array
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isInstalled() {
+        return $this->installed;
+    }
+
+    /**
+     * @param string|array $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @param bool $installed
+     */
+    public function setInstalled(bool $installed)
+    {
+        $this->installed = $installed;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 }

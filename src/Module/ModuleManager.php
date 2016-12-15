ede85e2ebd22aba9c8a57a1c726806def6b6e243
twilroad <269044570@qq.com>
2016-12-15 17:49:25 +0800

    handler module list

--- src/Module/ModuleManager.php
+++ src/Module/ModuleManager.php
@@ -10,6 +10,9 @@ namespace Notadd\Foundation\Module;
 
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 
 /**
  * Class ModuleManager.
@@ -17,7 +20,7 @@ use Illuminate\Events\Dispatcher;
 class ModuleManager
 {
     /**
-     * @var \Illuminate\Container\Container
+     * @var \Illuminate\Container\Container|\Notadd\Foundation\Application
      */
     protected $container;
 
@@ -27,14 +30,64 @@ class ModuleManager
     protected $events;
 
     /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * @var \Illuminate\Support\Collection
+     */
+    protected $modules;
+
+    /**
      * ModuleManager constructor.
      *
-     * @param \Illuminate\Container\Container $container
-     * @param \Illuminate\Events\Dispatcher   $events
+     * @param \Illuminate\Container\Container   $container
+     * @param \Illuminate\Events\Dispatcher     $events
+     * @param \Illuminate\Filesystem\Filesystem $files
      */
-    public function __construct(Container $container, Dispatcher $events)
+    public function __construct(Container $container, Dispatcher $events, Filesystem $files)
     {
         $this->container = $container;
         $this->events = $events;
+        $this->files = $files;
+        $this->modules = new Collection();
+    }
+
+    /**
+     * @param bool $installed
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function getModules($installed = false)
+    {
+        if ($this->modules->isEmpty()) {
+            if ($this->files->isDirectory($this->getModulePath()) && !empty($directories = $this->files->directories($this->getModulePath()))) {
+                (new Collection($directories))->each(function ($directory) use ($installed) {
+                    if ($this->files->exists($file = $directory . DIRECTORY_SEPARATOR . 'composer.json')) {
+                        $package = new Collection(json_decode($this->files->get($file), true));
+                        if (Arr::get($package, 'type') == 'notadd-extension' && $name = Arr::get($package, 'name')) {
+                            $module = new Module($name, Arr::get($package, 'authors'), Arr::get($package, 'description'));
+                            if($installed) {
+                                $module->setInstalled($installed);
+                            }
+                            $this->modules->put($directory, $module);
+                        }
+                    }
+                });
+            }
+        }
+
+        return $this->modules;
+    }
+
+    /**
+     * Module path.
+     *
+     * @return string
+     */
+    public function getModulePath()
+    {
+        return $this->container->basePath() . DIRECTORY_SEPARATOR . 'modules';
     }
 }

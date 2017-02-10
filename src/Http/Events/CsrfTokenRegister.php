26b5a628b08b20c393424c1670a0de8ae681f6f0
twilroad <269044470@qq.com>
2017-02-10 11:26:23 +0800

    add event CsrfTokenRegister

---
+++ src/Http/Events/CsrfTokenRegister.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-02-10 10:40
+ */
+namespace Notadd\Foundation\Http\Events;
+
+use Illuminate\Container\Container;
+use Notadd\Foundation\Http\Middlewares\VerifyCsrfToken;
+
+/**
+ * Class CsrfTokenRegister.
+ */
+class CsrfTokenRegister
+{
+    /**
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
+     * @var \Notadd\Foundation\Http\Middlewares\VerifyCsrfToken
+     */
+    protected $verifier;
+
+    /**
+     * CsrfTokenRegister constructor.
+     *
+     * @param \Illuminate\Container\Container                     $container
+     * @param \Notadd\Foundation\Http\Middlewares\VerifyCsrfToken $verifier
+     */
+    public function __construct(Container $container, VerifyCsrfToken $verifier)
+    {
+        $this->container = $container;
+        $this->verifier = $verifier;
+    }
+
+    /**
+     * Register except to verifier.
+     *
+     * @param $excepts
+     */
+    public function registerExcept($excepts)
+    {
+        $this->verifier->registerExcept($excepts);
+    }
+}

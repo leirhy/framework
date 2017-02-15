123dee7858fd16216e9e3a354d66e9732c865e40
qiyueshiyi <qiyueshiyi@outlook.com>
2017-02-15 11:48:16 +0800

    Add addPermission function Permission model, fix Permission table name wrong

--- src/Member/Permission.php
+++ src/Member/Permission.php
@@ -11,11 +11,38 @@ namespace Notadd\Foundation\Member;
 
 use Notadd\Foundation\Database\Model;
 
+/**
+ * Class Permission
+ *
+ * @property integer             $id
+ * @property string              $name
+ * @property string              $display_name
+ * @property string              $description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ *
+ * @package Notadd\Foundation\Member
+ */
 class Permission extends Model
 {
-    protected $table = 'permission';
+    protected $table = 'permissions';
 
     protected $fillable = [
         'name', 'display_name', 'description',
     ];
+
+    public static function addPermission($name, $display_name = null, $description = null)
+    {
+        $permission = static::where('name', $name)->first();
+
+        if (! $permission || ! $permission->exists) {
+            $permission = new static(['name' => $name]);
+        }
+
+        $permission->display_name = $display_name;
+        $permission->description  = $description;
+        $permission->save();
+
+        return $permission;
+    }
 }

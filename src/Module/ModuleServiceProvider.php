a68a5d3eab4fd90fc297fc0efee9112213ca5a09
twilroad <269044570@qq.com>
2017-03-02 14:43:15 +0800

    refactor module loading on boot

--- src/Module/ModuleServiceProvider.php
+++ src/Module/ModuleServiceProvider.php
@@ -8,7 +8,9 @@
  */
 namespace Notadd\Foundation\Module;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use Notadd\Foundation\Module\Commands\GenerateCommand;
 use Notadd\Foundation\Module\Commands\ListCommand;
@@ -21,13 +23,49 @@ use Notadd\Foundation\Module\Listeners\RouteRegister;
 class ModuleServiceProvider extends ServiceProvider
 {
     /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * ModuleServiceProvider constructor.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     * @param \Illuminate\Filesystem\Filesystem            $files
+     */
+    public function __construct(Application $app, Filesystem $files)
+    {
+        parent::__construct($app);
+        $this->files = $files;
+    }
+
+    /**
      * Boot service provider.
      */
     public function boot()
     {
-        collect($this->app->make('module')->getModules())->each(function (Module $module, $path) {
-            if ($this->app->make('files')->isDirectory($path) && is_string($module->getEntry())) {
-                $this->app->register($module->getEntry());
+        $this->app->make('module')->getModules()->each(function (Module $module, $path) {
+            if ($this->files->isDirectory($path)) {
+                if(is_string($module->getEntry()) && class_exists($module->getEntry())) {
+                    $this->app->register($module->getEntry());
+                } else {
+                }
+            }
+            if ($this->files->isDirectory($path) && is_string($module->getEntry())) {
+                if (class_exists($module->getEntry())) {
+                    $this->app->register($module->getEntry());
+                } else {
+                    if ($this->files->exists($autoload = $path . DIRECTORY_SEPARATOR . 'vendor' .DIRECTORY_SEPARATOR . 'autoload.php')) {
+                        $this->files->requireOnce($autoload);
+                        if (class_exists($module->getEntry())) {
+                            $this->app->register($module->getEntry());
+                        } else {
+                            throw new \Exception('Module load fail!');
+                        }
+                    } else {
+                        throw new \Exception('Module load fail!');
+                    }
+                }
             }
         });
         $this->app->make(Dispatcher::class)->subscribe(CsrfTokenRegister::class);

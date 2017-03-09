5cc3d7a40111578265230a63b2c4727328754328
twilroad <269044570@qq.com>
2017-03-09 19:36:23 +0800

    clear codes for module mail

--- src/Mail/Controllers/MailController.php
+++ src/Mail/Controllers/MailController.php
@@ -13,26 +13,11 @@ use Notadd\Foundation\Mail\Handlers\TestHandler;
 use Notadd\Foundation\Routing\Abstracts\Controller;
 use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
+/**
+ * Class MailController.
+ */
 class MailController extends Controller
 {
-    /**
-     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
-     */
-    protected $settings;
-
-    /**
-     * WatermarkController constructor.
-     *
-     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
-     *
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     */
-    public function __construct(SettingsRepository $settings)
-    {
-        parent::__construct();
-        $this->settings = $settings;
-    }
-
     /**
      * Api handler.
      *

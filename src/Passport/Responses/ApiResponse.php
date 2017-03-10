787e975c12cf8342cefe1624bd4fde24e725a2e1
twilroad <269044570@qq.com>
2017-03-10 16:21:37 +0800

    make status to 500 while status is bigger then 599

--- src/Passport/Responses/ApiResponse.php
+++ src/Passport/Responses/ApiResponse.php
@@ -34,6 +34,7 @@ class ApiResponse
         is_null($response) && $response = new Response();
         $params && $this->params = array_merge($this->params, $params);
         $status = collect($this->params)->get('code', 200);
+        $status > 598 && $status = 500;
         $response = $response->withStatus($status)
             ->withHeader('pragma', 'no-cache')
             ->withHeader('cache-control', 'no-store')

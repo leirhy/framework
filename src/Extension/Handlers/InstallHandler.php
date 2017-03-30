7cd204a96a519306ece2c0095ec8fa2e09f9708f
twilroad <269044570@qq.com>
2017-03-30 21:40:59 +0800

    add logic of extension install

--- src/Extension/Handlers/InstallHandler.php
+++ src/Extension/Handlers/InstallHandler.php
@@ -8,10 +8,15 @@
  */
 namespace Notadd\Foundation\Extension\Handlers;
 
+use Closure;
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Console\Kernel;
 use Notadd\Foundation\Extension\Abstracts\Installer;
 use Notadd\Foundation\Extension\ExtensionManager;
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
  * Class InstallHandler.
@@ -24,27 +29,22 @@ class InstallHandler extends SetHandler
     protected $manager;
 
     /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $settings;
+
+    /**
      * InstallHandler constructor.
      *
-     * @param \Illuminate\Container\Container $container
-     * @param \Notadd\Foundation\Extension\ExtensionManager $manager
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Extension\ExtensionManager           $manager
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
      */
-    public function __construct(Container $container, ExtensionManager $manager)
+    public function __construct(Container $container, ExtensionManager $manager, SettingsRepository $settings)
     {
         parent::__construct($container);
         $this->manager = $manager;
-    }
-
-    /**
-     * Errors for handler.
-     *
-     * @return array
-     */
-    public function errors()
-    {
-        return [
-            $this->translator->trans(''),
-        ];
+        $this->settings = $settings;
     }
 
     /**
@@ -54,16 +54,33 @@ class InstallHandler extends SetHandler
      */
     public function execute()
     {
-        $extension = $this->manager->get($this->request->input('name'));
-        if ($extension && method_exists($provider = $extension->getEntry(), 'install') && $class = call_user_func([
+        $extension = $this->manager->get($this->request->input('identification'));
+        if ($extension && method_exists($provider = $extension->getEntry(), 'install') && $closure = call_user_func([
                 $provider,
                 'install',
             ])
         ) {
-            if (class_exists($class)) {
-                $installer = $this->container->make($class);
-                if ($installer instanceof Installer) {
-                    return $installer->install();
+            if ($closure instanceof Closure) {
+                if ($this->settings->get('extension.' . $extension->getIdentification() . '.installed', false)) {
+                    $this->errors->push('模块标识[]已经被占用，如需继续安装，请卸载同标识插件！');
+
+                    return false;
+                }
+                $this->container->getProvider($provider) || $this->container->register($provider);
+                if ($closure()) {
+                    $input = new ArrayInput([
+                        '--force' => true,
+                    ]);
+                    $output = new BufferedOutput();
+                    $this->getConsole()->find('migrate')->run($input, $output);
+                    $this->getConsole()->find('vendor:publish')->run($input, $output);
+                    $log = explode(PHP_EOL, $output->fetch());
+                    $this->container->make('log')->info('install module:' . $extension->getIdentification(), $log);
+                    $this->data = $log;
+                    $this->messages->push('安装插件[' . $extension->getIdentification() . ']成功！');
+                    $this->settings->set('extension.' . $extension->getIdentification() . '.installed', true);
+
+                    return true;
                 }
             }
         }
@@ -72,14 +89,16 @@ class InstallHandler extends SetHandler
     }
 
     /**
-     * Messages for handler.
+     * Get console instance.
      *
-     * @return array
+     * @return \Illuminate\Contracts\Console\Kernel|\Notadd\Foundation\Console\Application
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
-    public function messages()
+    public function getConsole()
     {
-        return [
-            $this->translator->trans(''),
-        ];
+        $kernel = $this->container->make(Kernel::class);
+        $kernel->bootstrap();
+
+        return $kernel->getArtisan();
     }
 }

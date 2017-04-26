66e463c9bf2ebba606c54a55470b846cc0f92915
twilroad <269044570@qq.com>
2017-04-26 16:01:30 +0800

    add container to model

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -8,6 +8,7 @@
  */
 namespace Notadd\Foundation\Database;
 
+use Illuminate\Container\Container;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 /**
@@ -15,4 +16,19 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
  */
 class Model extends EloquentModel
 {
+    /**
+     * @var \Illuminate\Contracts\Container\Container|\Notadd\Foundation\Application
+     */
+    protected $container;
+
+    /**
+     * Model constructor.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->container = Container::getInstance();
+    }
 }

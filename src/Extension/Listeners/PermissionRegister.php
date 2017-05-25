3d56007b016433bce436fadbea2170528135048c
twilroad <269044570@qq.com>
2017-05-25 16:25:02 +0800

    add permission register for extension component

---
+++ src/Extension/Listeners/PermissionRegister.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime      2017-05-25 16:24
+ */
+namespace Notadd\Foundation\Extension\Listeners;
+
+use Notadd\Foundation\Permission\Abstracts\PermissionRegister as AbstractPermissionRegister;
+
+/**
+ * Class PermissionRegister.
+ */
+class PermissionRegister extends AbstractPermissionRegister
+{
+    /**
+     * Handle Permission Registrar.
+     */
+    public function handle()
+    {
+        // TODO: Implement handle() method.
+    }
+}

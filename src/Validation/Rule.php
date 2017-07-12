ecef1c6bea39e1016c8f519f99d5fdfd04dec212
twilroad <269044570@qq.com>
2017-07-12 15:43:15 +0800

    refactor usage of validate rules

---
+++ src/Validation/Rule.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-07-12 14:16
+ */
+namespace Notadd\Foundation\Validation;
+
+use Illuminate\Validation\Rule as IlluminateRule;
+
+/**
+ * Class Rule.
+ */
+class Rule extends IlluminateRule
+{
+    /**
+     * @return string
+     */
+    public static function image()
+    {
+        return 'image';
+    }
+
+    /**
+     * @return string
+     */
+    public static function numeric()
+    {
+        return 'required';
+    }
+
+    /**
+     * @return string
+     */
+    public static function required()
+    {
+        return 'required';
+    }
+}

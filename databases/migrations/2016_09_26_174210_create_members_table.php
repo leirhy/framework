0e8c0bb64c02df1290d55610116bb03ede2d85e0
twilroad <269044570@qq.com>
2017-08-01 00:32:31 +0800

    set email field can be null

--- databases/migrations/2016_09_26_174210_create_members_table.php
+++ databases/migrations/2016_09_26_174210_create_members_table.php
@@ -22,7 +22,7 @@ class CreateMembersTable extends Migration
         $this->schema->create('members', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

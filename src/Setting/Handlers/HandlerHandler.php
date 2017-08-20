c7f84748b7ec8210142adf71c8c5fed501eb803c
twilroad <269044570@qq.com>
2017-08-20 21:06:19 +0800

    fix error of enable multidomain configuration to take effect

--- src/Setting/Handlers/HandlerHandler.php
+++ src/Setting/Handlers/HandlerHandler.php
@@ -45,12 +45,13 @@ class HandlerHandler extends Handler
         ], [
             'name.value.required' => '网站名称必须填写',
         ]);
-        $this->settings->set('site.enabled', $this->request->input('enabled.value', false));
-        $this->settings->set('site.name', $this->request->input('name.value', ''));
-        $this->settings->set('site.domain', $this->request->input('domain.value', ''));
         $this->settings->set('site.beian', $this->request->input('beian.value', ''));
         $this->settings->set('site.company', $this->request->input('company.value', ''));
         $this->settings->set('site.copyright', $this->request->input('copyright.value', ''));
+        $this->settings->set('site.domain', $this->request->input('domain.value', ''));
+        $this->settings->set('site.enabled', $this->request->input('enabled.value', false));
+        $this->settings->set('site.multidomain', $this->request->input('multidomain.value', false));
+        $this->settings->set('site.name', $this->request->input('name.value', ''));
         $this->settings->set('site.statistics', $this->request->input('statistics.value', ''));
         $this->withCode(200)->withMessage('修改设置成功！');
     }

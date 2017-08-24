1adb33ccd4be825c1138762e9dfc6bd4a9f2e0f4
twilroad <269044570@qq.com>
2017-08-24 16:02:12 +0800

    refactor logic of install and uninstall

--- src/Module/Handlers/InstallHandler.php
+++ src/Module/Handlers/InstallHandler.php
@@ -9,10 +9,14 @@
 namespace Notadd\Foundation\Module\Handlers;
 
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Support\Collection;
 use Notadd\Foundation\Module\Abstracts\Installer;
 use Notadd\Foundation\Module\ModuleManager;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
  * Class InstallHandler.
@@ -25,15 +29,22 @@ class InstallHandler extends Handler
     protected $manager;
 
     /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $setting;
+
+    /**
      * InstallHandler constructor.
      *
-     * @param \Illuminate\Container\Container         $container
-     * @param \Notadd\Foundation\Module\ModuleManager $manager
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Module\ModuleManager                 $manager
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $setting
      */
-    public function __construct(Container $container, ModuleManager $manager)
+    public function __construct(Container $container, ModuleManager $manager, SettingsRepository $setting)
     {
         parent::__construct($container);
         $this->manager = $manager;
+        $this->setting = $setting;
     }
 
     /**
@@ -41,26 +52,77 @@ class InstallHandler extends Handler
      */
     public function execute()
     {
-        $result = false;
+        set_time_limit(0);
         $module = $this->manager->get($this->request->input('identification'));
-        if ($module->offsetExists('installer')) {
-            $installer = $module->get('installer');
-            if (($installer = $this->container->make($installer)) instanceof Installer) {
-                $installer->setModule($module);
-                if ($installer->install()) {
-                    $result = true;
-                } else {
-                    $this->code = 500;
+        $output = new BufferedOutput();
+        $result = false;
+        if ($module) {
+            $collection = collect();
+            // Has Installer.
+            $module->offsetExists('installer') && tap($collection, function (Collection $collection) use ($module) {
+                if (class_exists($installer = $module->get('installer'))) {
+                    $collection->put('installer', $this->container->make($installer));
                 }
-                $this->parseInfo($installer->info());
-                $this->container->make('log')->info('install data:', $this->data->toArray());
+            });
+            // Has Migration.
+            $module->offsetExists('migrations') && $collection->put('migrations', $module->get('migrations'));
+            // Query Collection.
+            if ($collection->count() && $collection->every(function ($instance, $key) use ($module, $output) {
+                    switch ($key) {
+                        case 'migrations':
+                            if (is_array($instance) && collect($instance)->every(function ($path) use ($module, $output) {
+                                    $path = $module->get('directory') . DIRECTORY_SEPARATOR . $path;
+                                    $migration = str_replace($this->container->basePath(), '', $path);
+                                    $migration = trim($migration, DIRECTORY_SEPARATOR);
+                                    $input = new ArrayInput([
+                                        '--path'  => $migration,
+                                        '--force' => true,
+                                    ]);
+                                    $this->getConsole()->find('migrate')->run($input, $output);
+
+                                    return true;
+                                })) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                            break;
+                        case 'uninstall':
+                            if ($instance instanceof Installer && $instance->install()) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                            break;
+                        default:
+                            return false;
+                            break;
+                    }
+                })) {
+                $result = true;
             }
         }
         if ($result) {
+            $this->container->make('log')->info('Install Module ' . $this->request->input('identification') . ':', explode(PHP_EOL, $output->fetch()));
+            $this->setting->set('module.' . $module->identification() . '.installed', true);
             $this->withCode(200)->withMessage('安装模块成功！');
         } else {
             $this->withCode(500)->withError('安装模块失败！');
         }
+    }
+
+    /**
+     * Get console instance.
+     *
+     * @return \Illuminate\Contracts\Console\Kernel|\Notadd\Foundation\Console\Application
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    protected function getConsole()
+    {
+        $kernel = $this->container->make(Kernel::class);
+        $kernel->bootstrap();
+
+        return $kernel->getArtisan();
     }
 
     protected function parseInfo(Collection $data) {
--- src/Module/Handlers/UninstallHandler.php
+++ src/Module/Handlers/UninstallHandler.php
@@ -9,10 +9,14 @@
 namespace Notadd\Foundation\Module\Handlers;
 
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Support\Collection;
 use Notadd\Foundation\Module\Abstracts\Uninstaller;
 use Notadd\Foundation\Module\ModuleManager;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
  * Class UninstallHandler.
@@ -25,15 +29,22 @@ class UninstallHandler extends Handler
     protected $manager;
 
     /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $setting;
+
+    /**
      * UninstallHandler constructor.
      *
-     * @param \Illuminate\Container\Container $container
-     * @param \Notadd\Foundation\Module\ModuleManager $manager
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Module\ModuleManager                 $manager
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $setting
      */
-    public function __construct(Container $container, ModuleManager $manager)
+    public function __construct(Container $container, ModuleManager $manager, SettingsRepository $setting)
     {
         parent::__construct($container);
         $this->manager = $manager;
+        $this->setting = $setting;
     }
 
     /**
@@ -42,21 +53,57 @@ class UninstallHandler extends Handler
     public function execute()
     {
         set_time_limit(0);
-        $result = false;
         $module = $this->manager->get($this->request->input('identification'));
-        if ($module && method_exists($provider = $module->service(), 'uninstall')) {
-            if (($uninstaller = $this->container->make(call_user_func([$provider, 'uninstall']))) instanceof Uninstaller) {
-                $uninstaller->setModule($module);
-                if ($uninstaller->uninstall()) {
-                    $result = true;
-                } else {
-                    $this->code = 500;
+        $output = new BufferedOutput();
+        $result = false;
+        if ($module) {
+            $collection = collect();
+            // Has Uninstaller.
+            $module->offsetExists('uninstaller') && tap($collection, function (Collection $collection) use ($module) {
+                if (class_exists($uninstaller = $module->get('uninstaller'))) {
+                    $collection->put('uninstall', $this->container->make($uninstaller));
                 }
-                $this->parseInfo($uninstaller->info());
-                $this->container->make('log')->info('uninstall data:', $this->data->toArray());
+            });
+            // Has Migrations.
+            $module->offsetExists('migrations') && $collection->put('migrations', $module->get('migrations'));
+            if ($collection->count() && $collection->every(function ($instance, $key) use ($module, $output) {
+                    switch ($key) {
+                        case 'migrations':
+                            if (is_array($instance) && collect($instance)->every(function ($path) use ($module, $output) {
+                                    $path = $module->get('directory') . DIRECTORY_SEPARATOR . $path;
+                                    $migration = str_replace($this->container->basePath(), '', $path);
+                                    $migration = trim($migration, DIRECTORY_SEPARATOR);
+                                    $input = new ArrayInput([
+                                        '--path'  => $migration,
+                                        '--force' => true,
+                                    ]);
+                                    $this->getConsole()->find('migrate:rollback')->run($input, $output);
+
+                                    return true;
+                                })) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                            break;
+                        case 'uninstall':
+                            if ($instance instanceof Uninstaller && $instance->uninstall()) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                            break;
+                        default:
+                            return false;
+                            break;
+                    }
+                })) {
+                $result = true;
             }
         }
         if ($result) {
+            $this->container->make('log')->info('Uninstall Module ' . $this->request->input('identification') . ':', explode(PHP_EOL, $output->fetch()));
+            $this->setting->set('module.' . $module->identification() . '.installed', false);
             $this->withCode(200)->withMessage('卸载模块成功！');
         } else {
             $this->withCode(500)->withError('卸载模块失败！');
@@ -64,9 +111,24 @@ class UninstallHandler extends Handler
     }
 
     /**
+     * Get console instance.
+     *
+     * @return \Illuminate\Contracts\Console\Kernel|\Notadd\Foundation\Console\Application
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    protected function getConsole()
+    {
+        $kernel = $this->container->make(Kernel::class);
+        $kernel->bootstrap();
+
+        return $kernel->getArtisan();
+    }
+
+    /**
      * @param \Illuminate\Support\Collection $data
      */
-    protected function parseInfo(Collection $data) {
+    protected function parseInfo(Collection $data)
+    {
         $data->has('data') && $this->data = collect($data->get('data'));
         $data->has('errors') && $this->errors = collect($data->get('errors'));
         $data->has('messages') && $this->messages = collect($data->get('messages'));

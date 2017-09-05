a1fd64e03e134c4969a7bfda147001a1d98cc56d
twilroad <269044570@qq.com>
2017-09-05 17:09:43 +0800

    add informations data to category info

--- src/Http/Middlewares/AssetsPublish.php
+++ src/Http/Middlewares/AssetsPublish.php
@@ -51,7 +51,7 @@ class AssetsPublish
      */
     public function handle($request, Closure $next)
     {
-        if (file_exists($file = $this->container->staticPath() . DIRECTORY_SEPARATOR . $path) && !Str::endsWith('/', $request->path())) {
+        if (file_exists($file = $this->container->staticPath() . DIRECTORY_SEPARATOR . $request->path()) && !Str::endsWith('/', $request->path())) {
             $headers = [];
             switch (pathinfo($file, PATHINFO_EXTENSION)) {
                 case 'css':

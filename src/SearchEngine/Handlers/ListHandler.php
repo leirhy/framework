a9e4a62a232eb5a2980df24017c13b6e027ae7d1
twilroad <269044570@qq.com>
2017-09-12 18:47:09 +0800

    add logic of seo rule list which filter by identification

--- src/SearchEngine/Handlers/ListHandler.php
+++ src/SearchEngine/Handlers/ListHandler.php
@@ -8,7 +8,11 @@
  */
 namespace Notadd\Foundation\SearchEngine\Handlers;
 
+use Illuminate\Container\Container;
+use Notadd\Foundation\Module\ModuleManager;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\SearchEngine\Models\Rule as SeoRule;
+use Notadd\Foundation\Validation\Rule;
 
 /**
  * Class ListHandler.
@@ -16,12 +20,45 @@ use Notadd\Foundation\Routing\Abstracts\Handler;
 class ListHandler extends Handler
 {
     /**
+     * @var bool
+     */
+    protected $onlyValues = true;
+
+    /**
+     * @var \Notadd\Foundation\Module\ModuleManager
+     */
+    protected $module;
+
+    /**
+     * ListHandler constructor.
+     *
+     * @param \Illuminate\Container\Container         $container
+     * @param \Notadd\Foundation\Module\ModuleManager $module
+     */
+    public function __construct(Container $container, ModuleManager $module)
+    {
+        parent::__construct($container);
+        $this->module = $module;
+    }
+
+    /**
      * Execute Handler.
      *
      * @throws \Exception
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        list($identification) = $this->validate($this->request, [
+            'identification' => Rule::required(),
+        ], [
+            'identification.required' => '模块标识必须填写',
+        ]);
+        if ($this->module->has($identification)) {
+            $builder = SeoRule::query();
+            $builder->where('module', $identification);
+            $this->withCode(200)->withData($builder->get())->withMessage('获取数据成功！');
+        } else {
+            $this->withCode(500)->withError('模块不存在！');
+        }
     }
 }
--- src/SearchEngine/Listeners/RouterRegister.php
+++ src/SearchEngine/Listeners/RouterRegister.php
@@ -22,9 +22,10 @@ class RouterRegister extends AbstractRouteRegister
     public function handle()
     {
         $this->router->group(['middleware' => ['auth:api', 'cross', 'web'], 'prefix' => 'api/administration/seo'], function () {
-            $this->router->post('/', SeoController::class . '@list');
+            $this->router->post('/', SeoController::class . '@seo');
             $this->router->post('create', SeoController::class . '@create');
             $this->router->post('edit', SeoController::class . '@edit');
+            $this->router->post('list', SeoController::class . '@list');
             $this->router->post('order', SeoController::class . '@order');
             $this->router->post('module', SeoController::class . '@module');
             $this->router->post('remove', SeoController::class . '@remove');

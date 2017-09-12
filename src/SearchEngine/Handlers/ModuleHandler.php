6b09f8126c4a035c77a472985894052b118ff822
twilroad <269044570@qq.com>
2017-09-12 15:22:18 +0800

    add logic of module list for seo manager

--- src/SearchEngine/Handlers/ModuleHandler.php
+++ src/SearchEngine/Handlers/ModuleHandler.php
@@ -8,7 +8,11 @@
  */
 namespace Notadd\Foundation\SearchEngine\Handlers;
 
+use Illuminate\Container\Container;
+use Notadd\Foundation\Module\Module;
+use Notadd\Foundation\Module\ModuleManager;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
 /**
  * Class ModuleHandler.
@@ -16,12 +20,50 @@ use Notadd\Foundation\Routing\Abstracts\Handler;
 class ModuleHandler extends Handler
 {
     /**
+     * @var \Notadd\Foundation\Module\ModuleManager
+     */
+    protected $module;
+
+    /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $setting;
+
+    /**
+     * ModuleHandler constructor.
+     *
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Module\ModuleManager                 $module
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $setting
+     */
+    public function __construct(Container $container, ModuleManager $module, SettingsRepository $setting)
+    {
+        parent::__construct($container);
+        $this->module = $module;
+        $this->setting = $setting;
+    }
+
+    /**
      * Execute Handler.
      *
      * @throws \Exception
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $modules = collect();
+        $this->module->getEnabledModules()->each(function (Module $module) use ($modules) {
+            $modules->put($module->identification(), [
+                'alias'          => $this->setting->get('module.' . $module->identification() . '.domain.alias', ''),
+                'identification' => $module->identification(),
+                'name'           => $module->get('name'),
+                'order'          => 0,
+            ]);
+        });
+        $modules->put('global', [
+            'alias'          => '/',
+            'identification' => 'global',
+            'name'           => '全局',
+        ]);
+        $this->withCode(200)->withData($modules)->withMessage('获取模块列表成功！');
     }
 }

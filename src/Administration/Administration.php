759efd6e2dceb9953439373720d196afba93aab1
twilroad <269044570@qq.com>
2017-09-27 13:49:27 +0800

    remove unnecessary IoC injection

--- src/Administration/Administration.php
+++ src/Administration/Administration.php
@@ -8,8 +8,6 @@
  */
 namespace Notadd\Foundation\Administration;
 
-use Illuminate\Container\Container;
-use Illuminate\Events\Dispatcher;
 use InvalidArgumentException;
 use Notadd\Foundation\Administration\Abstracts\Administrator;
 use Notadd\Foundation\Routing\Traits\Helpers;
@@ -25,28 +23,6 @@ class Administration
      * @var \Notadd\Foundation\Administration\Abstracts\Administrator
      */
     protected $administrator;
-
-    /**
-     * @var \Illuminate\Container\Container
-     */
-    protected $container;
-
-    /**
-     * @var \Illuminate\Events\Dispatcher
-     */
-    protected $events;
-
-    /**
-     * Administration constructor.
-     *
-     * @param \Illuminate\Container\Container $container
-     * @param \Illuminate\Events\Dispatcher   $events
-     */
-    public function __construct(Container $container, Dispatcher $events)
-    {
-        $this->container = $container;
-        $this->events = $events;
-    }
 
     /**
      * Get administrator.
--- src/Administration/AdministrationServiceProvider.php
+++ src/Administration/AdministrationServiceProvider.php
@@ -33,8 +33,8 @@ class AdministrationServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('administration', function ($app) {
-            return new Administration($app, $app['events']);
+        $this->app->singleton('administration', function () {
+            return new Administration();
         });
     }
 }

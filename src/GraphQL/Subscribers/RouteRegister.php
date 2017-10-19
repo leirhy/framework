39ce5bc7c264d9586909da46d63f1bd62b784045
twilroad <269044570@qq.com>
2017-10-19 15:55:54 +0800

    add router and controller logic for graphql

---
+++ src/GraphQL/Subscribers/RouteRegister.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        TwilRoad <heshudong@ibenchu.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime      2017-10-19 15:44
+ */
+namespace Notadd\Foundation\GraphQL\Subscribers;
+
+use Notadd\Foundation\GraphQL\Controllers\GraphQLController;
+use Notadd\Foundation\Routing\Abstracts\RouteRegister as AbstractRouteRegister;
+
+/**
+ * Class RouteRegister.
+ */
+class RouteRegister extends AbstractRouteRegister
+{
+    /**
+     * Handle Route Register.
+     */
+    public function handle()
+    {
+        $this->router->group(['middleware' => ['auth:api', 'cross', 'web']], function () {
+            $this->router->any('graphql', GraphQLController::class . '@query');
+        });
+    }
+}

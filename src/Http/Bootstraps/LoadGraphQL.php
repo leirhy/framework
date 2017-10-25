b317db2aac6e460ab2dd4a1432d23c70be8eb4ed
twilroad <269044570@qq.com>
2017-10-25 10:39:22 +0800

    add logic of graphql loading

--- src/Http/Bootstraps/LoadGraphQL.php
+++ src/Http/Bootstraps/LoadGraphQL.php
@@ -10,12 +10,15 @@ namespace Notadd\Foundation\Http\Bootstraps;
 
 use Notadd\Foundation\Application;
 use Notadd\Foundation\Http\Contracts\Bootstrap;
+use Notadd\Foundation\Routing\Traits\Helpers;
 
 /**
  * Class LoadGraphQL.
  */
 class LoadGraphQL implements Bootstrap
 {
+    use Helpers;
+
     /**
      * Bootstrap the given application.
      *
@@ -23,6 +26,45 @@ class LoadGraphQL implements Bootstrap
      */
     public function bootstrap(Application $application)
     {
-        // TODO: Implement bootstrap() method.
+        $paths = collect();
+        $directories = $this->file->directories($this->container->frameworkPath('src'));
+        foreach ($directories as $directory) {
+            $graphql = realpath($directory . DIRECTORY_SEPARATOR . 'GraphQL');
+            if ($this->file->isDirectory($graphql)) {
+                $data = [];
+                $data['graphql'] = '\\Notadd\\Foundation\\' . $this->file->name($directory) . '\\GraphQL';
+                $mutation = $graphql . DIRECTORY_SEPARATOR . 'Mutations';
+                if ($this->file->isDirectory($mutation)) {
+                    foreach ($this->file->files($mutation) as $file) {
+                        $class = $data['graphql'] . '\\Mutations\\' . $this->file->name($file);
+                        if ($this->file->extension($file) == 'php' && class_exists($class)) {
+                            $data['mutation'][] = $class;
+                        }
+                    }
+                }
+                $query = $graphql . DIRECTORY_SEPARATOR . 'Queries';
+                if ($this->file->isDirectory($query)) {
+                    foreach ($this->file->files($query) as $file) {
+                        $class = $data['graphql'] . '\\Queries\\' . $this->file->name($file);
+                        if ($this->file->extension($file) == 'php' && class_exists($class)) {
+                            $data['query'][] = $class;
+                        }
+                    }
+                }
+                $type = $graphql . DIRECTORY_SEPARATOR . 'Types';
+                if ($this->file->isDirectory($type)) {
+                    foreach ($this->file->files($type) as $file) {
+                        $class = $data['graphql'] . '\\Types\\' . $this->file->name($file);
+                        if ($this->file->extension($file) == 'php' && class_exists($class)) {
+                            $data['type'][] = $class;
+                        }
+                    }
+                }
+                $paths->push($data);
+            }
+        }
+        $paths->each(function ($definition) {
+            isset($definition['type']) && $this->graphql->addType($definition['type']);
+        });
     }
 }

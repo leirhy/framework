60104665bca9ce03f3f9f0c92466bef5fcd1debf
twilroad <269044570@qq.com>
2017-11-01 15:55:53 +0800

    add mutation of setting to graphql

--- configurations/graphql.php
+++ configurations/graphql.php
@@ -15,6 +15,7 @@ return [
                 'clearCache'  => \Notadd\Foundation\Cache\GraphQL\Mutations\ClearMutation::class,
                 'extensions'  => \Notadd\Foundation\Extension\GraphQL\Mutations\ExtensionMutation::class,
                 'modules'     => \Notadd\Foundation\Module\GraphQL\Mutations\ModuleMutation::class,
+                'setting'     => \Notadd\Foundation\Setting\GraphQL\Mutations\SettingMutation::class,
                 'settings'    => \Notadd\Foundation\Setting\GraphQL\Mutations\SettingsMutation::class,
             ],
             'query'    => [

e4c926e12d463e33a058fd270177973c518fccf2
twilroad <269044570@qq.com>
2017-11-03 16:39:06 +0800

    finish logic for query of module

--- src/Module/GraphQL/Queries/ModuleQuery.php
+++ src/Module/GraphQL/Queries/ModuleQuery.php
@@ -11,12 +11,32 @@ namespace Notadd\Foundation\Module\GraphQL\Queries;
 use GraphQL\Type\Definition\ListOfType;
 use GraphQL\Type\Definition\Type;
 use Notadd\Foundation\GraphQL\Abstracts\Query;
+use Notadd\Foundation\Module\Module;
 
 /**
  * Class ConfigurationQuery.
  */
 class ModuleQuery extends Query
 {
+    /**
+     * @return array
+     */
+    public function args()
+    {
+        return [
+            'enabled'   => [
+                'defaultValue' => null,
+                'name' => 'enabled',
+                'type' => Type::boolean(),
+            ],
+            'installed' => [
+                'defaultValue' => null,
+                'name' => 'installed',
+                'type' => Type::boolean(),
+            ],
+        ];
+    }
+
     /**
      * @param $root
      * @param $args
@@ -25,7 +45,26 @@ class ModuleQuery extends Query
      */
     public function resolve($root, $args)
     {
-        return $this->module->repository()->toArray();
+        if ($args['enabled'] === true) {
+            $collection = $this->module->enabled();
+        } elseif ($args['installed'] === true) {
+            $collection = $this->module->installed();
+        } elseif ($args['installed'] === false) {
+            $collection = $this->module->notInstalled();
+        } else {
+            $collection = $this->module->repository();
+        }
+
+        return $collection->map(function (Module $module) {
+            $authors = (array)$module->get('author');
+            $string = $authors[0] ?? '';
+            $string .= ' <';
+            $string .= $authors[1] ?? '';
+            $string .= '>';
+            $module->offsetSet('author', $string);
+
+            return $module;
+        })->toArray();
     }
 
     /**

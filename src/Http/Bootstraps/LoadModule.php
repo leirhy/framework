1caad189d01aacb0940e465708029b23b6b4166b
twilroad <269044570@qq.com>
2017-11-07 17:13:07 +0800

    inject graphql for module

--- src/Http/Bootstraps/LoadModule.php
+++ src/Http/Bootstraps/LoadModule.php
@@ -39,7 +39,33 @@ class LoadModule implements Bootstrap
                             break;
                     }
                 });
-                $providers = collect($this->container->make('config')->get('app.providers'));
+                $schemas = $module->get('graphql.schemas', []);
+                foreach ($schemas as $key => $value) {
+                    if (isset($value['query']) && is_array($value['query']) && count($value['query']) > 0) {
+                        $key = 'graphql.schemas.' . $key . '.query';
+                        $queries = collect($this->config->get($key, []));
+                        if ($queries->isEmpty()) {
+                            $queries = collect($value['query']);
+                        } else {
+                            $queries = $queries->merge((array)$value['query']);
+                        }
+                        $this->config->set($key, $queries->toArray());
+                    }
+                    if (isset($value['mutation']) && is_array($value['mutation']) && count($value['mutation']) > 0) {
+                        $key = 'graphql.schemas.' . $key . '.mutation';
+                        $mutations = collect($this->config->get($key, []));
+                        if ($mutations->isEmpty()) {
+                            $mutations = collect($value['query']);
+                        } else {
+                            $mutations = $mutations->merge((array)$value['query']);
+                        }
+                        $this->config->set($key, $mutations->toArray());
+                    }
+                }
+                $types = collect($this->config->get('graphql.types'));
+                $types = $types->merge($module->get('graphql.types', []));
+                $this->config->set('graphql.types', $types->toArray());
+                $providers = collect($this->config->get('app.providers'));
                 $providers->push($module->service());
                 collect($module->get('providers', []))->each(function ($provider) use ($providers) {
                     $providers->push($provider);

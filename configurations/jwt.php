70c0378616b5472c63f006fbdd06062744333645
twilroad <269044570@qq.com>
2017-11-24 18:25:53 +0800

    switch to hs384

--- configurations/jwt.php
+++ configurations/jwt.php
@@ -15,7 +15,7 @@ return [
     ],
     'ttl'                    => env('JWT_TTL', 60),
     'refresh_ttl'            => env('JWT_REFRESH_TTL', 20160),
-    'algo'                   => env('JWT_ALGO', 'ES256'),
+    'algo'                   => env('JWT_ALGO', 'HS384'),
     'required_claims'        => [
         'iss',
         'iat',

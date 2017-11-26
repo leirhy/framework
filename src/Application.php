35602572b5984eaa5a458bdbbeccda0e4c397aa7
twilroad <269044570@qq.com>
2017-11-26 21:50:26 +0800

    update version

--- src/Application.php
+++ src/Application.php
@@ -45,7 +45,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      *
      * @var string
      */
-    const VERSION = '0.15.10';
+    const VERSION = '0.15.20';
 
     /**
      * The base path for the Notadd installation.
